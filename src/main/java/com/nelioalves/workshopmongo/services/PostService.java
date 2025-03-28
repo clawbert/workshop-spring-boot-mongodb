135c98e530ad8dd4cec110e8181a0cdaec25ccb6
clawbert <clawbertpereira@gmail.com>
2025-03-28 10:29:51 -0400

    Consulta simples com @Query

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -28,7 +28,7 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String title) {
-        return postRepository.findByTitleContainingIgnoreCase(title);
+        return postRepository.searchTitle(title);
     }
 
 }
